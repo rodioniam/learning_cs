383b5d5be7f03e3f16caeec1ebff7710ed88b6bb
RodionIamshanov <roiamshanov@gmail.com>
2026-07-13 19:18:13 +0200

    add: first commit of the learning process - data types in C#

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Learning_C_;
+
+class Program
+{
+    static void Main(string[] args)
+    // так пишутся комментарии в C#. Метод main является обязательным и это точка входа в программу = она выполняется отсюда
+
+    /*
+    так пишется многострочный комментарий
+    */
+    {
+        Console.WriteLine("Hello, World!");
+        Console.ReadLine();
+    }
+}
