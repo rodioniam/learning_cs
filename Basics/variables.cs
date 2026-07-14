d7a7ebd4653f02d16f617116c22cd860604e60b8
RodionIamshanov <roiamshanov@gmail.com>
2026-07-14 18:51:50 +0200

    add: example of variables

---
+++ Basics/variables.cs
@@ -0,0 +1,21 @@
+
+int variable;
+
+variable = 10;
+
+Console.WriteLine(variable);
+
+// так-же можно сразу при объявлении присваивать значения
+
+string hello = "Hello world!";
+
+Console.WriteLine(hello);
+
+// можно объявить сразу несколько переменных с одним типом
+
+int a, b;
+
+a = 10;
+b = 5;
+
+Console.WriteLine(a+b);
