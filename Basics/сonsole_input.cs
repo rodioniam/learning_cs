88928e993cec8134f5db1dd5fbaa26a408d3febf
RodionIamshanov <roiamshanov@gmail.com>
2026-07-14 20:05:13 +0200

    add: console input example

---
+++ Basics/сonsole_input.cs
@@ -0,0 +1,13 @@
+namespace Learning_C_;
+
+public static class Console_Input
+{
+    public static void Test()
+    {
+        string data;
+
+        data = Console.ReadLine();
+
+        Console.WriteLine("Hello "+ data + "!");
+    }
+}
