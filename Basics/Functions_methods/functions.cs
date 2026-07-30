21812f1cce6252a248ec46ccf8ef07900cb535b6
RodionIamshanov <roiamshanov@gmail.com>
2026-07-30 19:30:42 +0200

    add: methods overload examples

--- Basics/Functions_methods/functions.cs
+++ Basics/Functions_methods/functions.cs
@@ -20,10 +20,33 @@ public static class Functions
 
     // можно использовать функции и внутри самого класса
     public static void PrintResult()
-    {
-        int result = Sum(2, 5);
+    {   
+        // теперь тут в зависимости от параметров внутри метода будет вызван подходящий
+        int result = Sum(1, 1, 3);
+        int result2 = Sum(3, 2);
 
-        Console.WriteLine(result);
+        Console.WriteLine($"Version 1: {result}");
+        Console.WriteLine($"Version 2: {result2}");
     }
+
+    // перегрузка методов
+
+    // у меня уже есть метод Sum, но я могу написать еще один с другим набором аргументов
+
+    /// <summary>
+    /// описание метода
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <param name="c"></param>
+    /// <returns>sum of a, b, c as int</returns>
+    public static int Sum(int a, int b, int c)
+    {
+        int result = a + b + c;
+
+        return result;
+    }
+
+    
     
 }
